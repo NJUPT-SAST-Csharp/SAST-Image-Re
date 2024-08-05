0412c4f063dc952f68831c1c7ae8a5b1f0e80a9e
Jagdender <3027297767@qq.com>
2024-08-05 21:17:28 +0800

    Update project structure and `css` content.

--- src/AppHost/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Frontend>("frontend");
-
-builder.AddProject<Projects.WebAPI>("webapi");
-
-builder.Build().Run();
--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,14 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
-
 // Add services to the container.
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
-app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 
